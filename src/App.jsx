4feddf8d397ab8e7a0c1cbf6c248c3a04d650cca
mirzoibodullaev <1bodullaevm11@gmail.com>
2024-02-22 15:45:19 +0500

    push project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import MainPage from "./components/MainPage/MainPage";
+
+function App() {
+    return (
+        <div className="container">
+            <MainPage />
+        </div>
+    );
+}
+
+export default App;
